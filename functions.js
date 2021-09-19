cc3605503092e2b433c2c0dc37119856df1d1f63
ThiagoMB60 <thiagomoreira60@gmail.com>
2021-09-19 02:52:18 -0300

    feat: montagem de tabela dinamica OK

--- functions.js
+++ functions.js
@@ -4,86 +4,114 @@ function criaObjGrafo(numVertices) {
   grafo.numVerticesArray = [numVertices][numVertices.length];
 }
 
-function createCellsHead(numVertices) {
-  for (var i = -1; i < numVertices; i++) {
-    var headRow = document.getElementById("thRow");
+function createCellsHead(rowPai, numVertices) {
+  for (var j = -1; j < numVertices; j++) {
+    var row = document.getElementById(rowPai);
     var th = document.createElement("th");
 
-    headRow.appendChild(th);
-    var vertice = "v" + i;
-    th.id = vertice;
-    if (i != -1) {
-      vertice = document.createTextNode(vertice);
-    } else {
+    row.appendChild(th);
+    var vertice = "v" + j;
+    th.id = rowPai + vertice;
+    if (j == -1) {
       vertice = document.createTextNode("X");
+      th.style.backgroundColor = "red";
+    } else {
+      vertice = document.createTextNode(vertice);
     }
     th.appendChild(vertice);
   }
 }
 
-function criaHeadRow(numVertices) {
-  var tableHead = document.getElementById("theadInput");
-  var headRow = document.createElement("tr");
+function criaRowHeader(headPai, numVertices) {
+  var tableHead = document.getElementById(headPai);
+  var Row = document.createElement("tr");
 
-  tableHead.appendChild(headRow);
-  headRow.id = "thRow";
-  createCellsHead(numVertices);
+  tableHead.appendChild(Row);
+  Row.id = tableHead.id + "tr";
+  createCellsHead(Row.id, numVertices);
 }
 
-function criaTableHeader(numVertices) {
-  var table = document.getElementById("inputTable");
+function criaTableHeader(tablePai, numVertices) {
+  var table = document.getElementById(tablePai);
   var tableHead = document.createElement("thead");
 
   table.appendChild(tableHead);
-  tableHead.id = "theadInput";
-  criaHeadRow(numVertices);
+  tableHead.id = tablePai + "thead";
+  criaRowHeader(tableHead.id, numVertices);
 }
 
-function criaTableBody(numVertices) {
-  for (var i = 0; i < numVertices; i++) {
-    var table = document.getElementById("inputTable");
-    var tableBody = document.createElement("tbody");
+function createCellsBody(rowPai, numVertices, linha) {
+  for (var i = -1; i < numVertices; i++) {
+    var row = document.getElementById(rowPai);
+    var td = document.createElement("td");
 
-    table.appendChild(tableBody);
-    tableBody.id = "th" + i;
-    tableBody.value = "v" + i;
+    row.appendChild(td);
+
+    if (i == -1) {
+      var vertice = "v" + linha;
+      td.id = row.id + vertice;
+      vertice = document.createTextNode(vertice);
+    } else {
+      vertice = document.createTextNode(1000000);
+    }
+    td.appendChild(vertice);
   }
 }
 
-function entradaMatriz(numVertices) {
-  var divEntrada = document.getElementById("entradaGrafo");
-  var inputTable = document.createElement("table");
-  var texto = document.createElement("p");
+function criaRowBody(headBodPai, numVertices) {
+  for (var i = 0; i < numVertices; i++) {
+    var tableHead = document.getElementById(headBodPai);
+    var Row = document.createElement("tr");
 
-  divEntrada.style.display = "block";
-
-  divEntrada.appendChild(texto);
-  texto.appendChild(
-    document.createTextNode(
-      "Por favor, informe os valores para a tabela de adjacencia a seguir:"
-    )
-  );
-  divEntrada.appendChild(inputTable);
-  inputTable.id = "inputTable";
-
-  criaTableHeader(numVertices);
-  criaTableBody(numVertices);
+    tableHead.appendChild(Row);
+    Row.id = tableHead.id + "tr" + i;
+    createCellsBody(Row.id, numVertices, i);
+  }
 }
 
-function confirmation(numVertices) {
+function criaTableBody(tablePai, numVertices) {
+  var table = document.getElementById(tablePai);
+  var tableBody = document.createElement("tbody");
+
+  table.appendChild(tableBody);
+  tableBody.id = table.id + "tbody";
+  criaRowBody(tableBody.id, numVertices);
+}
+
+function criaTabela(divPai, numVertices, enunciado) {
+  var div = document.getElementById(divPai);
+  var table = document.createElement("table");
+  var p = document.createElement("p");
+
+  div.style.display = "block";
+
+  div.appendChild(p);
+  p.appendChild(document.createTextNode(enunciado));
+  div.appendChild(table);
+  table.id = divPai + "Table";
+
+  criaTableHeader(table.id, numVertices);
+  criaTableBody(table.id, numVertices);
+}
+
+function confirmation(divPai, numVertices) {
   if (
     confirm(
       "O número de vértices digitado é " + numVertices + ", deseja confirmar?"
     )
   ) {
     criaObjGrafo(numVertices);
-    entradaMatriz(numVertices);
+    criaTabela(
+      divPai,
+      numVertices,
+      "Por favor, informe os valores para a tabela de adjacencia a seguir:"
+    );
   }
 }
 
-function informaVertices() {
+function informaVertices(divPai) {
   var numVertices = document.getElementById("numVertices").value;
   if (numVertices != "") {
-    confirmation(numVertices);
+    confirmation(divPai, numVertices);
   }
 }
